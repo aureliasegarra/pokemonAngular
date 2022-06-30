304e1896f361fe0849a036bf9406f9138a3be2bb
Aurelia Segarra <av.segarra@gmail.com>
2022-06-30 13:59:59 +0200

    WIP: Components and templates OK

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,20 +4,27 @@ import { POKEMONS } from './mock-pokemon-list';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Liste de Pokemons</h1>`
-     
+  templateUrl: 'app.component.html',  
 })
 
 
 export class AppComponent implements OnInit{
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined ;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon){
-    console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+  selectPokemon(pokemonId: string){
+    const id = +pokemonId;
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if (pokemon) {
+      console.log(`Vous avez cliqué sur le pokemon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log('Vous avez demandé un pokemon qui n\'existe pas');
+      this.pokemonSelected = pokemon;
+    }
   }
 }
